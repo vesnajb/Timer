0921739aec8817e218db80d4ead040097b0cd965
janakievska.vesna <janakievska.vesna@gmail.com>
2021-11-13 14:37:54 +0100

    Add project files.

---
+++ Timer/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.BackColor == Color.Red)
+            {
+                this.BackColor = Color.AntiqueWhite;
+            }
+            else
+            {
+                this.BackColor = Color.Red;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = DateTime.Now.ToString();
+            textBox1.Text = DateTime.Now.Second.ToString();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int hh = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute;
+            int ss = DateTime.Now.Second;
+
+            string time = "";
+            if (hh < 10)
+            {
+                time += "0" + hh;
+            }
+            else
+                time += hh;
+            time += ":";
+            if (mm < 10)
+            {
+                time += "0" + mm;
+            }
+            else
+                time += mm;
+            time += ":";
+            if (ss < 10)
+            {
+                time += "0" + ss;
+            }
+            else
+                time += ss;
+            textBox2.Text = time;
+        }
+    }
+}
